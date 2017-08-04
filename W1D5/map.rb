af29755a68ef0692154589a3131b77099268f019
Harsh <harsh@hkumar.me>
2017-08-03 19:53:15 -0700

    complete w1d5 homework

---
+++ W1D5/map.rb
@@ -0,0 +1,33 @@
+class Map
+
+  attr_reader :map
+
+  def initialize
+    @map = []
+  end
+
+  def assign(key,value)
+    if lookup(key).nil?
+      @map << [key,value]
+    else
+      @map.each do |pair|
+        if pair.first == key
+          pair.last = value
+          break
+        end
+      end
+    end
+  end
+
+  def lookup(key)
+    @map.each do |pair|
+      return pair if pair.first == key
+    end
+    nil
+  end
+
+  def show
+    dup_map = @map.dup
+  end
+
+end
---
+++ W1D5/queue.rb
@@ -0,0 +1,18 @@
+class Queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue.unshift(el)
+  end
+
+  def dequeue
+    @queue.pop
+  end
+
+  def show
+    dup_queue = @queue.dup
+  end
+
+end
