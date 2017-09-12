eef1f668930252ffab1ce56f6843d7ac8d996726
Harsh <harsh@hkumar.me>
2017-09-11 20:39:38 -0700

    Use Redux provided applyMiddleware

--- W7/D2/frontend/store/store.js
+++ W7/D2/frontend/store/store.js
@@ -1,12 +1,23 @@
 import { createStore } from 'redux';
 import RootReducer from '../reducers/root_reducer';
+import { applyMiddleware } from 'redux';
+
 
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(RootReducer, preloadedState);
+  const store = createStore(RootReducer, preloadedState, applyMiddleware(addLoggingToDispatch));
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
   return store;
 }
+
+const addLoggingToDispatch = (store) => (next) => (action) => {
+  let storeDispatch = store.dispatch;
+  console.log('Old State: ', store.getState());
+  console.log('Action: ', action);
+  next(action);
+  console.log('New State: ', action);
+};
+
 
 export default configureStore;
--- W7/D2/frontend/todo_redux.jsx
+++ W7/D2/frontend/todo_redux.jsx
@@ -7,7 +7,8 @@ import Root from './components/root';
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  const store = applyMiddlewares(configureStore(preloadedState), addLoggingToDispatch);
+    const store = configureStore(preloadedState);
+  // const store = applyMiddlewares(configureStore(preloadedState), addLoggingToDispatch);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
@@ -15,18 +16,18 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-const addLoggingToDispatch = (store) => (next) => (action) => {
-  let storeDispatch = store.dispatch;
-  console.log('Old State: ', store.getState());
-  console.log('Action: ', action);
-  next(action);
-  console.log('New State: ', action);
-};
+// const addLoggingToDispatch = (store) => (next) => (action) => {
+//   let storeDispatch = store.dispatch;
+//   console.log('Old State: ', store.getState());
+//   console.log('Action: ', action);
+//   next(action);
+//   console.log('New State: ', action);
+// };
 
-const applyMiddlewares = (store, ...middlewares) => {
-  let dispatch = store.dispatch;
-  middlewares.forEach((middleware)=>{
-    dispatch = middleware(store)(dispatch);
-  });
-  return Object.assign({}, store, { dispatch });
-};
+// const applyMiddlewares = (store, ...middlewares) => {
+//   let dispatch = store.dispatch;
+//   middlewares.forEach((middleware)=>{
+//     dispatch = middleware(store)(dispatch);
+//   });
+//   return Object.assign({}, store, { dispatch });
+// };
