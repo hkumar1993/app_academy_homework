55ff39f97fff00f4c600bf8389b07b7aed4913f2
Harsh <harsh@hkumar.me>
2017-09-11 20:18:24 -0700

    Create Logger using store.dispatch

--- W7/D2/frontend/todo_redux.jsx
+++ W7/D2/frontend/todo_redux.jsx
@@ -8,7 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
   const store = configureStore(preloadedState);
-
+  store.dispatch = addLoggingToDispatch(store);
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
+  console.log('test');
 });
+
+const addLoggingToDispatch = (store) => {
+  let storeDispatch = store.dispatch;
+  return (action) => {
+    console.log('Old State: ', store.getState());
+    console.log('Action: ', action);
+    storeDispatch(action);
+    console.log('New State: ', store.getState());
+  }
+}
